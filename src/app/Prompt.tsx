cf13a80f294ce04ad0494000b391f7209082885c
kor_ka <gleb.putintsev@openland.com>
2019-03-01 11:13:53 +0300

    chore(client+host): better prompt layout

--- src/app/Prompt.tsx
+++ src/app/Prompt.tsx
@@ -21,9 +21,9 @@ export class Prompt extends React.PureComponent<{}, { accepted?: boolean }> {
         return (
             <FlexLayout style={{ width: '100%', flexDirection: 'row', backgroundColor: 'black', justifyContent: 'center', alignItems: 'center' }}>
                 <div style={{ flexGrow: 1, margin: 20 }}>
-                    <span style={{ color: 'white' }}>By using this website you agree with our <a style={{ color: 'white' }} href="/legal/privacy-policy">Privacy Policy</a>, <a style={{ color: 'white' }} href="legal/terms-and-conditions">Terms &amp; Conditions</a> and <a style={{ color: 'white' }} href="legal/cookie-policy">Cookie Policy</a></span>
+                    <span style={{ color: 'white' }}>By using this website you agree with our <a style={{ color: 'white' }} href="/legal/privacy-policy">Privacy&nbsp;Policy</a>, <a style={{ color: 'white' }} href="legal/terms-and-conditions">Terms&nbsp;&amp;&nbsp;Conditions</a> and <a style={{ color: 'white' }} href="legal/cookie-policy">Cookie&nbsp;Policy</a></span>
                 </div>
-                <Button style={{ marginRight: 20, backgroundColor: 'black', color: 'white' }} onClick={this.onAccepted}>Got it!</Button>
+                <Button style={{ marginRight: 20, backgroundColor: 'black', color: 'white' }} onClick={this.onAccepted}>OK</Button>
             </FlexLayout>
         );
     }
