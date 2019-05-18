cb77daaf779d8120982e4ce3f185521be3780ae0
kor_ka <gleb.putintsev@openland.com>
2019-05-18 15:27:54 +0300

    fix(server): trying to sparate worker

--- server/worker.ts
+++ server/worker.ts
@@ -3,11 +3,14 @@ const PORT = process.env.PORT || 5000
 import { createServer } from 'http';
 import * as socketIo from 'socket.io';
 import { SocketListener } from './src/model/transport/SocketListener';
+import bodyParser = require('body-parser');
 
 //
 // Configure ws
 //
 let app = express();
+app.use(bodyParser.json())
+
 let server = createServer(app);
 let io = socketIo(server);
 
